49556af2fb40b162942f3882ae6a83d3230460d6
OlaE1 <oe21yg@brocku.ca>
2024-11-10 17:30:45 -0500

    Update Lab8cc2.js

--- Labs/Lab8/Lab8cc2.js
+++ Labs/Lab8/Lab8cc2.js
@@ -1,6 +1,5 @@
 //Sets the initial colours of the three elements on page load
 
-
 document.getElementById("blue").style.color= "blue";
 document.getElementById("green").style.color= "green";
 document.getElementById("red").style.color= "red";
