8b27b6678e05310430c89330b01555dbde0a75f7
OlaE1 <oe21yg@brocku.ca>
2024-11-10 17:02:16 -0500

    Chall1

---
+++ Labs/Lab8/scripts.js
@@ -0,0 +1,34 @@
+document.write("<p>Hello World!</p>");
+
+function changeText(){
+document.getElementById("initial").innerHTML = "Replaced the text";
+}
+
+function changeTextBack(){
+  document.getElementById("initial").innerHTML = "Text will change here";
+}
+
+function fancifyText(){
+  document.getElementById("one").style.fontSize = "30px";
+  document.getElementById("one").style.color = "orange";
+  document.getElementById("two").style.marginLeft = "150px";
+  document.getElementById("two").style.color = "blue";
+  document.getElementById("three").style.textTransform = "uppercase";
+  document.getElementById("three").style.color = "red";
+  document.getElementById("one").style.color = "green";
+}
+//this is a comment
+
+var mathX;
+mathX = 5;
+var mathY = 7;
+var someText = "This is a text variable";
+
+function mathExample(x,y){
+  mathZ = x + y;
+  document.getElementById("mathResult").innerHTML = mathZ;
+  console.log("X is equal to: " + x);
+  console.log("Y is equal to: " + y);
+  console.log("Z is equal to: " + mathY);
+
+}
