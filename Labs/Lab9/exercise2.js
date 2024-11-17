2bc8417915123960f10865e5f7888c2763465f3a
OlaE1 <oe21yg@brocku.ca>
2024-11-17 18:51:36 -0500

    exc2

--- Labs/Lab9/exercise2.js
+++ Labs/Lab9/exercise2.js
@@ -1,12 +1,11 @@
 
-
-var myPrompt = prompt("What is your age?");
-var age = Number(myPrompt);
-console.log(myPrompt);
-
 var todayFullDate = new Date();
 var todayFullYear = todayFullDate.getFullYear();
+var myPrompt = prompt("What is your age?");
+var age = parseInt(myPrompt);
 var birthYearOutput = todayFullYear - age;
 
+console.log(myPrompt);
 console.log(todayFullYear);
+
 document.getElementById("birthYear").innerHTML = birthYearOutput;
