b7c230f16d3734f29ee4535f59b82f3c48309eda
OlaE1 <oe21yg@brocku.ca>
2024-12-11 16:27:55 -0500

    assignment 4

--- Assignments/Assignment4/assign4.js
+++ Assignments/Assignment4/assign4.js
@@ -1,8 +1,20 @@
 
-function multiply() {
-  var num1 = Number(document.getElementById("numOne").value);
-  var num2 = Number(document.getElementById("numTwo").value);
-  var multi = num1 * num2;
-  document.getElementById('prices').innerHTML = multi;
-  //document.write(multi);
+function multiply(){
+  var number1 = document.getElementById("numOne").value;
+  var number2 = document.getElementById("numTwo").value;
+  var multi = Math.floor(number1 * number2);
+
+  document.getElementById('prices').innerHTML = "$" + multi;
+  //document.write(times);
+  console.log(multi);
+}
+
+
+function showTheDate(){
+  var todayFullDate = new Date();
+  var todayFullYear = todayFullDate.getFullYear();
+  var todayMonth = todayFullDate.getMonth(); //fix for zero-based month index
+  var todayDate = todayFullDate.getDate();
+  document.getElementById('theDate').innerHTML = todayFullDate;
+  console.log(todayFullDate);
 }
